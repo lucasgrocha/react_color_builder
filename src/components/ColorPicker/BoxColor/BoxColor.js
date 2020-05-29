542a1c397a1ee8a15a78f8d3f048262adaf3bb04
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-05-29 15:20:26 -0300

    add copy to clipboard functionality

--- src/components/ColorPicker/BoxColor/BoxColor.js
+++ src/components/ColorPicker/BoxColor/BoxColor.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
 import classes from './BoxColor.module.css'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import styled from 'styled-components'
+
+const StyledCopyToClipBoard = styled(CopyToClipboard)`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 const boxColor = props => {
   const rgb = `rgb(${props.colors['red']}, ${props.colors['green']}, ${props.colors['blue']})`
 
   return (
     <div style={{backgroundColor: rgb}} className={classes.BoxColor}>
-      {rgb}
+      <StyledCopyToClipBoard text={rgb} onCopy={() => alert('Copied to clipboard')}>
+          <span>{rgb}</span>
+      </StyledCopyToClipBoard>
     </div>
   )
 }
