9a6537bec5e2f9ab90be65297a0cefd2eff59eae
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-05-30 13:12:36 -0300

    add animate.css to notification

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import classes from './Notification.module.css'
+import 'animate.css'
 
 const Notification = props => {
   const [disposed, setDisposed] = useState(false)
@@ -11,7 +12,7 @@ const Notification = props => {
 
   return (
     !disposed ?
-    <div className={classes.Notification} onClick={() => handleDisposeNotification() }>
+    <div className={`animate__animated animate__fadeInUp ${classes.Notification}`} onClick={() => handleDisposeNotification() }>
       <h1>{props.title || 'Title'}</h1>
       <h3>{props.content || 'Content'}</h3>
     </div>
